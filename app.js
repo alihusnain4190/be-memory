59cdab736e38c4d9d3b5c3f374cf40e5d5950234
alihusnain4190 <alihusnain4190@gmail.com>
2020-11-26 11:24:33 +0000

    add link in cors

--- app.js
+++ app.js
@@ -8,8 +8,18 @@ const {
   handle500,
 } = require("./errors/");
 const app = express();
-
-app.use(cors());
+const whiteList = [
+  "https://be-memory.herokuapp.com/api/f_imgs",
+  "https://be-memory.herokuapp.com/api/image",
+];
+const corsOption = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) return callback(null, true);
+    callback(new Error("Not allowed cors"));
+  },
+};
+app.use(cors(corsOption));
 app.use(express.json());
 
 app.get("/products/:id", function (req, res, next) {
